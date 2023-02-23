a6e8e8995ce775cf3a2985f3508455df39dd7d02
imperial-coder <okenedaniel66@gmail.com>
2023-02-23 15:52:56 -0800

    solution to task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	itn i = 48;
+
+	while (i < 58)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i++);
+	}
+	_putchar('\n');
+}
