213c87e8572147c74ff3aa6eaed38d3cbdae394b
imperial-coder <okenedaniel66@gmail.com>
2023-02-22 18:00:14 -0800

    made few changes to task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,9 +13,9 @@ void times_table(void)
 
 	for (i = 0; i < 9; i++)
 	{
-		_putchar(start);
-		for (j = 1; j < 9; j++)
+		for (j = 1; j < 10; j++)
 		{
+			_putchar(start + '0');
 			_putchar(',');
 			_putchar(' ');
 			if ((i * j) < 10)
