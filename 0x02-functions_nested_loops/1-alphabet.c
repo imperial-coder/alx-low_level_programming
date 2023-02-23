caacde5b0e66efda27e03e64229173791ea386d3
imperial-coder <okenedaniel66@gmail.com>
2023-02-22 16:23:09 -0800

    solution for task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		_putchar(i++)
+	}
+	_putchar('\n');
+}
