a50ec5af75128fbe277bef58eb6a43d71cfdb44b
imperial-coder <okenedaniel66@gmail.com>
2023-02-27 19:09:36 -0800

    solution to task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 
 /*Function prototypes */
+int _putchar(char s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif /*MAIN_H*/
