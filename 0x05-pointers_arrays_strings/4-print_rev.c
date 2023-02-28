e9420ea5d3858033d731ff674b2c549d501e8410
imperial-coder <okenedaniel66@gmail.com>
2023-02-27 19:22:17 -0800

    solution to tsk 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: pointer to string to be reversed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	while (*s)
+		s++;
+
+	s--;
+	
+	while (*s)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
