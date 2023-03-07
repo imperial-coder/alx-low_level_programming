d1b8a4ef7920936514c5b244a5d8ffa7e643b1e4
imperial-coder <okenedaniel66@gmail.com>
2023-03-06 17:42:51 -0800

    solution to task 2

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * strspn - gets the lenght of a prefix substring
+ * @s: string to be searched
+ * @accept: string to search
+ *
+ * Return: number of bytes in the inital segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
