a53c068a355d7f0f9e9e62b60fd7ea37852ad54b
imperial-coder <okenedaniel66@gmail.com>
2023-03-06 16:20:11 -0800

    solution to task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area.
+ * @daet: copied memory detsination
+ * @src: memory source
+ * @n: number of memory byte to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
