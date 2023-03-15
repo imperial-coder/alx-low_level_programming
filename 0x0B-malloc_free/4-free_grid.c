35a0591a80004dd52e86a7ce862d8b1a7e612461
imperial-coder <okenedaniel66@gmail.com>
2023-03-14 19:47:01 -0700

    solution for tak 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+i#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_grid -  frees a 2 dimensional grid previously created
+ * @grid: pointer to grid
+ * @height: height of grid
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i, j;
+
+	if (!grid || height <= 0)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
