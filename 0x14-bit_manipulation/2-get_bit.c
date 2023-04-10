4acedbab74f80b1fc446754d0bcdedbc02779622
imperial-coder <okenedaniel66@gmail.com>
2023-04-10 00:32:55 -0700

    solution for task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to convert to binary
+ * @index: index to find
+ *
+ * Return: value of index searched
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int index_val;
+
+	if (index > 63)
+		return(-1);
+	index_val = (n >> index) & 1;
+	
+	return(index_value);
+}
